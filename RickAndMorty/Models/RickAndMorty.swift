b8894fe9fab501acf26017827df5b8fa154e6f95
Пахаев Хусейн <rvoltigo@gmail.com>
2023-05-14 18:16:35 +0300

    With alamofire

--- RickAndMorty/Models/RickAndMorty.swift
+++ RickAndMorty/Models/RickAndMorty.swift
@@ -10,11 +10,42 @@ import Foundation
 struct RickAndMorty: Decodable {
     let info: Info
     var results: [Character]
+    
+    init(info: [String: Any], results: [Character]) {
+        self.info = Info(from: info)
+        self.results = results
+    }
+    
+    init(info: Info, results: [Character]) {
+        self.info = info
+        self.results = results
+    }
+    
+    static func getRickAndMorty(from value: Any) -> RickAndMorty {
+        guard let charactersData = value as? [String: Any] else {
+            return RickAndMorty(info: [:], results: [])
+        }
+        
+        guard let info = charactersData["info"] as? [String: Any] else {            return RickAndMorty(info: [:], results: [])
+        }
+        guard let characters = charactersData["results"] as? [[String: Any]] else {
+            return RickAndMorty(info: [:], results: [])
+        }
+        
+        return RickAndMorty(info: info, results: characters.map{ Character(from: $0)})
+    }
 }
 
 struct Info: Decodable {
     let next: String?
-    let prev: String?
+    
+    init(next: String?) {
+        self.next = next
+    }
+    
+    init(from characterData: [String: Any]) {
+        next = characterData["next"] as? String
+    }
 }
 
 struct Character: Decodable {
@@ -38,10 +69,61 @@ struct Character: Decodable {
         Origin: \(origin.name)
         """
     }
+    
+    init(
+        id: Int,
+        name: String,
+        status: String,
+        species: String,
+        gender: String,
+        origin: Origin,
+        location: Origin,
+        image: String,
+        episode: [String]
+    ) {
+        self.id = id
+        self.name = name
+        self.status = status
+        self.species = species
+        self.gender = gender
+        self.origin = origin
+        self.location = location
+        self.image = image
+        self.episode = episode
+    }
+    
+    init(from characterData: [String: Any]) {
+        id = characterData["id"] as? Int ?? 0
+        name = characterData["name"] as? String ?? ""
+        status = characterData["status"] as? String ?? ""
+        species = characterData["species"] as? String ?? ""
+        gender = characterData["gender"] as? String ?? ""
+        origin = Origin(from: characterData["origin"] as? [String: Any] ?? [:])
+        location = Origin(from: characterData["location"] as? [String: Any] ?? [:])
+        image = characterData["image"] as? String ?? ""
+        episode = characterData["episode"] as? [String] ?? [""]
+    }
+    
+    static func getCharacter(from value: Any) -> Character {
+        guard let charactersData = value as? [String: Any] else {
+            return Character(from: [:])
+        }
+        
+        return Character(from: charactersData)
+    }
+    
 }
 
 struct Origin: Decodable {
     let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    init(from originData: [String: Any]) {
+        name = originData["name"] as? String ?? ""
+    }
 }
 
 struct Episode: Decodable {
@@ -57,11 +139,26 @@ struct Episode: Decodable {
         """
     }
     
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case date = "air_date"
-        case episode = "episode"
-        case characters = "characters"
+    init(name: String, date: String, episode: String, characters: [String]) {
+        self.name = name
+        self.date = date
+        self.episode = episode
+        self.characters = characters
+    }
+    
+    init(from episodesData: [String: Any]) {
+        name = episodesData["name"] as? String ?? ""
+        date = episodesData["air_date"] as? String ?? ""
+        episode = episodesData["episode"] as? String ?? ""
+        characters = episodesData["characters"] as? [String] ?? [""]
+    }
+    
+    static func getEpisode(from value: Any) -> Episode {
+        guard let episodeData = value as? [String: Any] else {
+            return Episode(from: [:])
+        }
+        
+        return Episode(from: episodeData)
     }
 }
 
